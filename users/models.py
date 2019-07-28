ef69e43c455e697f84633825f69fc5c15477a999
xieshouquan <xieshouquan@p1902.com>
2019-07-28 22:23:26 +0800

    修复版本小问题

--- users/models.py
+++ users/models.py
@@ -27,16 +27,16 @@ class MyUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 class UserProfile(AbstractBaseUser):
-    username=models.CharField("用户名",unique=True,max_length=30)
-    nick_name=models.CharField("昵称",max_length=50,default="")
-    birday=models.DateField("生日",null=True,blank=True)
-    gender=models.CharField("性别",max_length=10,choices=(("male","男"),("female","女")),default="female")
-    address=models.CharField("联系地址",max_length=100,default="")
-    mobile=models.CharField("电话",max_length=11,null=True,blank=True)
-    image=models.ImageField("头像",upload_to="image/%Y/%m",default="image/default.png",max_length=100)
+    username=models.CharField(verbose_name="用户名",unique=True,max_length=30)
+    nick_name=models.CharField(verbose_name="昵称",max_length=50,default="")
+    birday=models.DateField(verbose_name="生日",null=True,blank=True)
+    gender=models.CharField(verbose_name="性别",max_length=10,choices=(("male","男"),("female","女")),default="female")
+    address=models.CharField(verbose_name="联系地址",max_length=100,default="")
+    mobile=models.CharField(verbose_name="电话",max_length=11,null=True,blank=True)
+    image=models.ImageField(verbose_name="头像",upload_to="image/%Y/%m",default="image/default.png",max_length=100)
 
-    creat_time=models.DateTimeField("注册时间",auto_now_add=True)
-    update_time=models.DateTimeField("更新时间",auto_now=True)
+    creat_time=models.DateTimeField(verbose_name="注册时间",auto_now_add=True)
+    update_time=models.DateTimeField(verbose_name="更新时间",auto_now=True)
     is_active = models.BooleanField(default=True)
     is_admin=models.BooleanField(default=True)
     is_delete=models.BooleanField(default=False,verbose_name='逻辑删除', help_text='逻辑删除')
@@ -75,8 +75,8 @@ class UserProfile(AbstractBaseUser):
 
 
 class EmailVerifyRecord(models.Model):
-    code=models.CharField("验证码",max_length=20)
-    email=models.EmailField("邮箱",max_length=50)
+    code=models.CharField(verbose_name="验证码",max_length=20)
+    email=models.EmailField(verbose_name="邮箱",max_length=50)
     send_type=models.CharField(choices=(("register","注册"),("forget","找回密码")),max_length=10)
     send_time=models.DateTimeField(auto_now=True)
 
@@ -86,11 +86,11 @@ class EmailVerifyRecord(models.Model):
 
 
 class Banner(models.Model):
-    title=models.CharField("标题",max_length=100)
-    image=models.ImageField("轮播图",upload_to="banner/%Y/%m",max_length=100)
-    url=models.URLField("访问地址",max_length=200)
-    index=models.IntegerField("顺序",default=100)
-    add_time=models.DateTimeField("添加时间",auto_now=True)
+    title=models.CharField(verbose_name="标题",max_length=100)
+    image=models.ImageField(verbose_name="轮播图",upload_to="banner/%Y/%m",max_length=100)
+    url=models.URLField(verbose_name="访问地址",max_length=200)
+    index=models.IntegerField(verbose_name="顺序",default=100)
+    add_time=models.DateTimeField(verbose_name="添加时间",auto_now=True)
 
     class Meta:
         verbose_name="轮播图"
