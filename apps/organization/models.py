cf37e78ee614244c92d699b40ee45254eed2c80c
xieshouquan <xieshouquan@p1902.com>
2019-08-19 14:59:03 +0800

    课程机构主体功能完成

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -20,7 +20,7 @@ class CityDict(models.Model):
 class CourseOrg(models.Model):
     name = models.CharField(verbose_name="机构名称", max_length=50)
     desc = models.TextField(verbose_name="机构描述")
-    categorg=models.CharField(max_length=20,default='pxjg',choices=(('pxjg','培训机构'),('gr','个人'),('gx','高校')),verbose_name='机构类别')
+    category=models.CharField(max_length=20,default='pxjg',choices=(('pxjg','培训机构'),('gr','个人'),('gx','高校')),verbose_name='机构类别')
     click_nums = models.IntegerField(verbose_name="点击数", default=0)
     fav_nums = models.IntegerField(verbose_name="收藏数", default=0)
     image = models.ImageField(verbose_name="封面图", upload_to="org/%Y/%m", max_length=100)
@@ -47,6 +47,7 @@ class Teacher(models.Model):
     points = models.CharField(verbose_name="教学特点", max_length=50)
     click_nums = models.IntegerField(verbose_name="点击数", default=0)
     fav_nums = models.IntegerField(verbose_name="收藏数", default=0)
+    image = models.ImageField(verbose_name="头像", upload_to="teacher/%Y/%m", max_length=100,default='')
     create_time = models.DateTimeField(verbose_name="添加时间", auto_now_add=True)
 
     class Meta:
--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, re_path, include
 
-from organization.views import OrgView, AddUserAskView
+from organization.views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, \
+    AddFavView
 
 urlpatterns = [
-    # 课程机构列表页
+    # 课程机构
     re_path(r'^list/$', OrgView.as_view(), name='list'),
     re_path(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
+    re_path(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='home'),
+    re_path(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='course'),
+    re_path(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name='desc'),
+    re_path(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='teacher'),
+
+    # 机构收藏
+    re_path(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
 ]
